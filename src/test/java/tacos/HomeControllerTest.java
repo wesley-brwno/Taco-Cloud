00b16705d53f68e7a3e1203fa84f21c8da18ee9a
wesley <brunow9@outlook.com>
2023-06-30 11:57:52 -0300

    Criação da homepage e sua Controller

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tacos.controller.HomeController;
 
 import static org.hamcrest.Matchers.containsString;
 
