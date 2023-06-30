2a41e93c2f9d5e81fca07fa2b3ac7c6889d6cfc6
wesley <brunow9@outlook.com>
2023-06-30 13:47:18 -0300

    Adição de novas páginas e suas controllers

--- src/main/java/tacos/controller/DesignTacoController.java
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -3,10 +3,7 @@ package tacos.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import tacos.domain.Ingredient;
 import tacos.domain.Ingredient.Type;
 import tacos.domain.Taco;
@@ -68,4 +65,10 @@ public class DesignTacoController {
                 .collect(Collectors.toList());
     }
 
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
 }
